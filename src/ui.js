339564ecef3a301212a8015d04d3cfb3eff9756b
Matthew Peters <mattsterp@gmail.com>
2018-12-07 13:44:07 -0500

    Adding a post and the post submitted alert box.

--- src/ui.js
+++ src/ui.js
@@ -5,7 +5,7 @@ class UI {
     this.bodyInput = document.querySelector('#body');
     this.idInput = document.querySelector('#id');
     this.postSubmit = document.querySelector('.post-submit');
-    this.formState = 'add';
+    this.forState = 'add';
   }
 
   showPosts(posts) {
@@ -13,22 +13,58 @@ class UI {
 
     posts.forEach(post => {
       output += `
-        <div class="card mb-3">
+          <div class="card mb-3">
             <div class="card-body">
-                <h4 class="post-title">${post.title}</h4>
-                <p class="card-text">${post.body}</p>
-                <a href="#" class="edit card-link" data-id="${post.id}">
-                    <i class="fa fa-pencil"></i>
-                </a> 
-                <a href="#" class="delete card-link" data-id="${post.id}">
-                    <i class="fa fa-remove"></i>
-                </a> 
+              <h4 class="card-title">${post.title}</h4>
+              <p class="card-text">${post.body}</p>
+              <a href="#" class="edit card-link" data-id="${post.id}">
+                <i class="fa fa-pencil"></i>
+              </a>
+  
+              <a href="#" class="delete card-link" data-id="${post.id}">
+              <i class="fa fa-remove"></i>
+            </a>
             </div>
-        </div>
+          </div>
         `;
     });
 
     this.post.innerHTML = output;
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert div
+    container.insertBefore(div, posts);
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
   }
 }
 
